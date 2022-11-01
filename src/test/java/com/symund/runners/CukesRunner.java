0820ea148add132ef18f269e13b723a722b28ab2
AlihanTamguc <alihanwit@gmail.com>
2022-11-01 21:03:48 +0100

    Framework is created

---
+++ src/test/java/com/symund/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.symund.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",
+        features = "src/test/resources/features",
+        glue = "com/symund/step_definitions",
+        dryRun = false,
+        tags = ""
+)
+
+
+
+
+
+public class CukesRunner {
+
+}
